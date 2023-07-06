ff19a9b403cf5b0cbd774e2e6f6ae2a716973464
Matheus Arantes <125316650+Programmerarantes@users.noreply.github.com>
2023-07-06 16:05:25 -0300

    avançando com js, encontrando bugs

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let color = "black"
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(16)
+
+    let btnPopup = document.querySelector("#popup") 
+    btnPopup.addEventListener("click", function() {
+        let size = getSize()
+        createBoard(size)
+    })
 })
 
 //creating a function to define the size of the board
@@ -11,10 +18,37 @@ function createBoard(size) {
 
     let numDivs = size * size
 
-    for(let i = 0; i<numDivs; i++) {
+    for(let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow"
+        div.addEventListener("mouseover", colorDiv())
         board.insertAdjacentElement("beforeend", div)
     }
 
+}
+//creating a function to get the size and check it
+function getSize() {
+    let input = prompt("Write the size of the board: ");
+    let message = document.querySelector("#message");
+    if (input == "") {
+        message.innerHTML = "Please provide a number";
+    } 
+    else if (input < 0 || input > 100) {
+        message.innerHtml = "The size should be between 1 and 100";
+    } 
+    else {
+        message.innerHTML = "Get Ready!";
+        return input;
+    }
+}
+
+function colorDiv() {
+    if(color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50% )`
+    } else {
+        this.style.backgroundColor = 'black'
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice
 }
